79d66abb0e32490550c65aaae3e86867e06c5caa
Elienai <elienay.soares07@gmail.com>
2023-05-16 00:08:56 -0300

    add migrations and connection MySQL

--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp;
+using PokemonReviewApp.Datas;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddTransient<Seed>();
+builder.Services.AddCors();
+
+builder.Services.AddDbContext<DataContext>(options => 
+  options.UseMySql(
+    builder.Configuration.GetConnectionString("DefaultConnection"),
+    new MySqlServerVersion(new Version(8, 0, 26))
+  ));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "This is my Web Api");
-
 app.Run();
