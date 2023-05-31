6b407c33f414ccb7d12ad3bf8ad1beb1747460c0
Elienai <elienay.soares07@gmail.com>
2023-05-31 11:40:54 -0300

    add repository, interface and controller folds

--- Program.cs
+++ Program.cs
@@ -1,20 +1,29 @@
 using Microsoft.EntityFrameworkCore;
 using PokemonReviewApp;
 using PokemonReviewApp.Datas;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddTransient<Seed>();
+builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
+
 builder.Services.AddCors();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<DataContext>(options => 
   options.UseMySql(
-    builder.Configuration.GetConnectionString("DefaultConnection"),
+    builder.Configuration.GetConnectionString("Tsundoku"),
     new MySqlServerVersion(new Version(8, 0, 26))
   ));
 
 var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapGet("/", () => "This is my Web Api");
 app.Run();
